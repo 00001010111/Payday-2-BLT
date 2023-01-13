ed198e97d7ae3666bd4195e0b5e2c10e9241f9d2
Eliosse <101139518+EliosseX@users.noreply.github.com>
2023-01-13 20:43:58 +0100

    Changed little things

--- src/console/console.h
+++ src/console/console.h
@@ -1,10 +1,5 @@
 #ifndef __CONSOLE_HEADER__
 #define __CONSOLE_HEADER__
-
-// Credit: Christopher Oicles
-// http://stackoverflow.com/questions/16500726/open-write-to-console-from-a-c-dll
-// Stack overflow post
-
 #include <stdio.h>
 
 class CConsole {
